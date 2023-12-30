2030b91ffec44ccc0efca17e9701dec1aa183ece
johnlinvc <johnlinvc@gmail.com>
2023-12-30 09:48:00 +0800

    Initial commit

---
+++ lib/mlir/dialect/ruby.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "ruby/version"
+
+module Mlir
+  module Dialect
+    module Ruby
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "mlir/dialect/ruby"
+
+require "minitest/autorun"
