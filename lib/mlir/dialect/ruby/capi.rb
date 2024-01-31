dd6d47a73801a46308212ae26972e743e55f9761
johnlinvc <johnlinvc@gmail.com>
2024-01-31 12:03:22 +0800

    support dynamically configure FFI lib name

--- lib/mlir/dialect/ruby/capi.rb
+++ lib/mlir/dialect/ruby/capi.rb
@@ -4,10 +4,11 @@ require "ffi"
 module MLIR
   module Dialect
     module Ruby
+      FFI_LIB_NAME = ENV["MLIR_RUBY_LIB_NAME"] || "RubyAllCAPILib"
       # CAPI wrapper
       module CAPI
         extend FFI::Library
-        ffi_lib "RubyAllCAPILib"
+        ffi_lib MLIR::Dialect::Ruby::FFI_LIB_NAME
         attach_function :mlirGetDialectHandle__ruby__, [], MLIR::CAPI::MlirDialectHandle.by_value
       end
     end
